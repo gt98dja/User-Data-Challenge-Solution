868b2b7069c1404763dce85df96b9b41f2a1e889
Daniel Jansson <danja612@MacBook-Pro.local>
2014-04-25 15:39:24 +0200

    Done adding tableView

--- User Data Challenge Solution/User Data Challenge Solution/ViewController.m
+++ User Data Challenge Solution/User Data Challenge Solution/ViewController.m
@@ -22,12 +22,31 @@
     self.users = [UserData users];
     NSLog(@"%@", self.users);
     
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"userCell" forIndexPath:indexPath];
+    cell.textLabel.text = self.users[indexPath.row][USER_USERNAME];
+    cell.detailTextLabel.text = self.users[indexPath.row][USER_EMAIL];
+    cell.imageView.image = self.users[indexPath.row][USER_PROFILE_PICTURE];
+
+    
+    return cell;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.users.count;
 }
 
 @end
